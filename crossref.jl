6e8d1680239a683ed5d775c667223a1ff700eec3
Jeremy Ottley <Gazaunga@gmail.com>
2023-07-23 11:20:05 -0400

    Create crossref.jl

---
+++ crossref.jl
@@ -0,0 +1,20 @@
+using HTTP
+using EzXML
+
+const URL::String = "https://doi.crossref.org/getPrefixPublisher/?prefix="
+
+function getPrefix(prefix)
+  resp = HTTP.get(URL * prefix)
+  return String(resp.body)
+end
+
+print("Enter a publisher prefix: ")
+userInput = readline()
+
+try 
+  doc = parsexml(getPrefix(userInput))
+  publishers = root(doc)
+  println("", first(nodecontent.(findall("//publisher_name", publishers))))
+catch exception
+  println(exception)
+end
